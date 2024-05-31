ecbf8912792c8b71b58ea66af904fb3aed233ad7
satorumaruyama <satolale77@gmail.com>
2024-05-31 10:29:54 +0900

    初稿前コミット

--- js/main.js
+++ js/main.js
@@ -1,12 +1,4 @@
-// const swiper = new Swiper('.swiper', {
-//     loop: true,
-//     autoplay: {
-//       delay: 5000,
-//     },
-//     speed: 1000,
-// });
-
-
+// ハンバーガーメニュー
 $(function () {
   $('#js-hamburger').on('click', function () {
     $('.header__navItems').slideToggle(100)
@@ -14,34 +6,33 @@ $(function () {
   });
 });
 
-$(function(){
-  $('a[href^="#"]').click(function(){
-    var speed = 500;
-    var href= $(this).attr("href");
-    var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top;
-    $("html, body").animate({scrollTop:position}, speed, "swing");
-    return false;
-  });
+// 費用コースタブ切り替え
+$('.tab_box .tab_btn').click(function() {
+  var index = $('.tab_box .tab_btn').index(this);
+  $('.tab_box .tab_btn, .tab_box .tab_panel').removeClass('active');
+  $(this).addClass('active');
+  $('.tab_box .tab_panel').eq(index).addClass('active');
 });
 
-$('.animated').waypoint({
-  handler (direction) {
-    if (direction === 'down') {
-      $(this.element).addClass('fadeIn');
-      this.destroy();
-    }
-  },
-  offset: '50%',
-});
 
-$('.faq__answer').hide();
-$('.faq__question').on('click', (e) => {
-  if($(e.target).hasClass('active')) {
-    $(e.target).removeClass('active');
-    $(e.target).next().hide();
-  } else {
-    $(e.target).addClass('active');
-    $(e.target).next().show();
-  }
-});
+
+// $(function(){
+//   $('a[href^="#"]').click(function(){
+//     var speed = 500;
+//     var href= $(this).attr("href");
+//     var target = $(href == "#" || href == "" ? 'html' : href);
+//     var position = target.offset().top;
+//     $("html, body").animate({scrollTop:position}, speed, "swing");
+//     return false;
+//   });
+// });
+
+// $('.animated').waypoint({
+//   handler (direction) {
+//     if (direction === 'down') {
+//       $(this.element).addClass('fadeIn');
+//       this.destroy();
+//     }
+//   },
+//   offset: '50%',
+// });
